0a14e471e8c88bea794386a7081cae563efa1258
Adam Paszke <apaszke@google.com>
2019-09-17 12:05:13 -0700

    Add the CFG rewrite that removes loops

--- Sources/LibShapeChecker/CFGPreprocessing.swift
+++ Sources/LibShapeChecker/CFGPreprocessing.swift
@@ -3,7 +3,7 @@ import SIL
 typealias BlockName = String
 
 extension Block {
-  var successors : [BlockName]? {
+  var successors: [BlockName]? {
     switch terminatorDef.terminator {
     case .return(_): return []
     case let .br(label, _): return [label]
@@ -26,7 +26,7 @@ extension Block {
 func induceReducibleCFG(_ blocks: [Block]) -> Bool? {
   var predecessors = DefaultDict<BlockName, Set<BlockName>>{ _ in [] }
   var successors = DefaultDict<BlockName, Set<BlockName>>{ _ in [] }
-  var numVertices = blocks.count
+  var blocksRemaining = blocks.count
   var toReduce = Set<BlockName>()
   let startBlock = blocks[0].identifier
 
@@ -47,7 +47,7 @@ func induceReducibleCFG(_ blocks: [Block]) -> Bool? {
   }
 
   while let block = toReduce.first {
-    assert(numVertices > 0)
+    assert(blocksRemaining > 0)
     toReduce.removeFirst()
     guard let parent = predecessors[block].only else {
       fatalError("Collapsing a block with multiple predecessors")
@@ -57,6 +57,7 @@ func induceReducibleCFG(_ blocks: [Block]) -> Bool? {
     successors[parent].remove(parent)
     for successor in successors[block] {
       predecessors[successor].remove(block)
+      // Avoid introducing self-loops
       if successor != parent {
         predecessors[successor].insert(parent)
       }
@@ -66,14 +67,14 @@ func induceReducibleCFG(_ blocks: [Block]) -> Bool? {
     }
     successors.remove(block)
     predecessors.remove(block)
-    numVertices -= 1
+    blocksRemaining -= 1
   }
 
 
-  return numVertices == 1
+  return blocksRemaining == 1
 }
 
-struct Loop: Equatable {
+class Loop {
   let header: BlockName
   var body: Set<BlockName> // NB: Doesn't include the header!
 
@@ -84,8 +85,8 @@ struct Loop: Equatable {
 }
 
 // PRECONDITION: induceReducibleCFG(blocks)
-func findLoops(_ blocks: [Block]) -> [Loop]? {
-  var blocksByName = blocks.reduce(into: [BlockName: Block]()) {
+func findLoops(_ blocks: [Block]) -> [Loop] {
+  let blocksByName = blocks.reduce(into: [BlockName: Block]()) {
     $0[$1.identifier] = $1
   }
   var predecessors = blocks.reduce(into: DefaultDict<BlockName, Set<BlockName>>{ _ in [] }) {
@@ -129,4 +130,130 @@ func findLoops(_ blocks: [Block]) -> [Loop]? {
   }
 
   return loops.dictionary.values.sorted(by: { ($0.body.count, $0.header) < ($1.body.count, $1.header) })
+}
+
+// PRECONDITION: induceReducibleCFG(blocks)
+func unloop(_ blocks: inout [Block]) {
+  var loops = findLoops(blocks)
+  guard !loops.isEmpty else { return }
+
+  // TODO: Make this function more hygenic. It's fine if we assume that
+  //       blocks follow the bbX convention, but this is not robust enough
+  //       for the general case.
+  let freshSuffix = count(from: 0) .>> { "_\($0)" }
+  var blocksByName = blocks.reduce(into: [BlockName: Block]()) {
+    $0[$1.identifier] = $1
+  }
+  func clone(_ blockName: BlockName, hint: String? = nil) -> Block {
+    let oldBlock = blocksByName[blockName]!
+    let name = blockName + (hint ?? "") + freshSuffix()
+    let newBlock = Block(name,
+                         oldBlock.arguments,
+                         oldBlock.operatorDefs,
+                         oldBlock.terminatorDef)
+    blocksByName[name] = newBlock
+    blocks.append(newBlock)
+    return newBlock
+  }
+
+  func unreachable(like blockName: BlockName) -> BlockName {
+    let oldBlock = blocksByName[blockName]!
+    let name = blockName + "_unreachable"
+    if blocksByName.keys.contains(name) {
+      return name
+    } else {
+      let unreachableBlock =
+        Block(name, oldBlock.arguments, [], TerminatorDef(.unreachable, nil))
+      blocksByName[name] = unreachableBlock
+      blocks.append(unreachableBlock)
+      return name
+    }
+  }
+
+  for loop in loops {
+    let bodyClones = loop.body.sorted().reduce(into: [BlockName: Block]()) {
+      $0[$1] = clone($1)
+    }
+    let entryPoints = blocksByName[loop.header]!.successors!.filter{ loop.body.contains($0) }
+
+    var bridgeHeader = clone(loop.header, hint: "_bridge")
+    // Rewrite the bridge header such that its arguments end up unused, while the original
+    // values are produced by special builtins which tell us nothing about the value.
+    bridgeHeader.operatorDefs.insert(
+      contentsOf: bridgeHeader.arguments.map{
+        OperatorDef(Result([$0.valueName]),
+                    .builtin("anyInhabitant", [], $0.type),
+                    nil) },
+      at: 0)
+    bridgeHeader.arguments = bridgeHeader.arguments.map{ Argument("%unused" + freshSuffix(), $0.type) }
+    // There are two kinds of outgoing edges from the bridge block:
+    //   - those that go into the loop are replaced to jump to the cloned body
+    //   - those that would skip the loop are replaced with unreachable blocks
+    //     (because we know that those paths are not taken).
+    substitute(inside: &bridgeHeader, labels: {
+      entryPoints.contains($0) ? bodyClones[$0]!.identifier : unreachable(like: $0)
+    })
+    // The first iteration of the loop should either exit or jump to the bridge header.
+    loop.body.forEach {
+      substitute(inside: &blocksByName[$0]!,
+                 labels: { $0 == loop.header ? bridgeHeader.identifier : $0 })
+    }
+
+    var finalHeader = clone(loop.header, hint: "_final")
+    // Final header always exits the loop, so the edges that go inside are unreachable.
+    substitute(inside: &finalHeader, labels: {
+      entryPoints.contains($0) ? unreachable(like: $0) : $0
+    })
+    bodyClones.values.forEach {
+      substitute(inside: &blocksByName[$0.identifier]!,
+                 labels: {
+        if $0 == loop.header {
+          // The clones should always jump to the final header instead of the original one.
+          return finalHeader.identifier
+        } else if loop.body.contains($0) {
+          // All jumps that stay inside the body should point to other clones.
+          return bodyClones[$0]!.identifier
+        } else {
+          // But all exits should stay untouched.
+          return $0
+        }
+      })
+    }
+
+    // Add the newly created blocks to each loop that contains this header
+    // (i.e. to all the outer loop of this one).
+    for otherLoop in loops {
+      guard otherLoop.header != loop.header,
+            otherLoop.body.contains(loop.header) else { continue }
+      otherLoop.body.formUnion(bodyClones.values.map{ $0.identifier })
+      otherLoop.body.insert(bridgeHeader.identifier)
+      otherLoop.body.insert(finalHeader.identifier)
+    }
+  }
+}
+
+func substitute(inside block: inout Block,
+                labels labelSub: (BlockName) -> BlockName) {
+  func replaceTerminator(_ terminator: Terminator) -> Terminator {
+    switch terminator {
+      case let .br(label, args):
+        return .br(labelSub(label), args)
+      case let .condBr(cond, trueLabel, trueArgs, falseLabel, falseArgs):
+        return .condBr(cond,
+                       labelSub(trueLabel), trueArgs,
+                       labelSub(falseLabel), falseArgs)
+      case let .switchEnum(operand, cases):
+        return .switchEnum(operand, cases.map {
+          switch $0 {
+          case let .case(declRef, label): return .case(declRef, labelSub(label))
+          case let .default(label): return .default(labelSub(label))
+          }
+        })
+      case .return(_): fatalError("return should never be part of a loop")
+      case .unreachable: fatalError("unreachable should never be part of a loop")
+      case .unknown(_): fatalError("attempting to transform CFGs with unknown terminators")
+    }
+  }
+  block.terminatorDef = TerminatorDef(replaceTerminator(block.terminatorDef.terminator),
+                                      block.terminatorDef.sourceInfo)
 }
--- Tests/ShapeCheckerTests/CFGPreprocessingTests.swift
+++ Tests/ShapeCheckerTests/CFGPreprocessingTests.swift
@@ -4,73 +4,78 @@ import XCTest
 
 let returnInstr = Terminator.return(Operand("", .namedType("")))
 
+let irreducible: [Block] = [
+  Block("bb0", .condBr("", "bb1", [], "bb2", [])),
+  Block("bb1", .condBr("", "bb2", [], "bb3", [])),
+  Block("bb2", .condBr("", "bb1", [], "bb3", [])),
+  Block("bb3", returnInstr),
+]
+
+let chain: [Block] = [
+  Block("bb0", .br("bb1", [])),
+  Block("bb1", .br("bb2", [])),
+  Block("bb2", .br("bb3", [])),
+  Block("bb3", .br("bb4", [])),
+  Block("bb4", returnInstr),
+]
+
+let ifDiamond: [Block] = [
+  Block("bb0", .condBr("", "bb1", [], "bb2", [])),
+  Block("bb1", .br("bb3", [])),
+  Block("bb2", .br("bb3", [])),
+  Block("bb3", returnInstr),
+]
+
+let simpleLoop: [Block] = [
+  Block("bb0", .br("bb1", [])),
+  Block("bb1", .condBr("", "bb2", [], "bb3", [])),
+  Block("bb2", .br("bb1", [])),
+  Block("bb3", returnInstr),
+]
+
+let loopWithIf: [Block] = [
+  Block("bb0", .br("bb1", [])),
+  Block("bb1", .condBr("", "bb2", [], "bb6", [])),
+  Block("bb2", .condBr("", "bb3", [], "bb4", [])),
+  Block("bb3", .br("bb5", [])),
+  Block("bb4", .br("bb5", [])),
+  Block("bb5", .br("bb1", [])),
+  Block("bb6", returnInstr),
+]
+
+let loopWithTwoBackEdges: [Block] = [
+  Block("bb0", .br("bb1", [])),
+  Block("bb1", .condBr("", "bb2", [], "bb5", [])),
+  Block("bb2", .condBr("", "bb3", [], "bb4", [])),
+  Block("bb3", .br("bb1", [])),
+  Block("bb4", .br("bb1", [])),
+  Block("bb5", returnInstr),
+]
+
+let nestedLoop: [Block] = [
+  Block("bb0", .br("bb1", [])),
+  Block("bb1", .condBr("", "bb2", [], "bb5", [])),
+  Block("bb2", .condBr("", "bb3", [], "bb4", [])),
+  Block("bb3", .br("bb2", [])),
+  Block("bb4", .br("bb1", [])),
+  Block("bb5", returnInstr),
+]
+
+let reducibleExamples: [[Block]] = [
+  chain,
+  ifDiamond,
+  simpleLoop,
+  loopWithIf,
+  loopWithTwoBackEdges,
+  nestedLoop,
+]
+
 final class CFGPreprocessingTests: XCTestCase {
-
-  let irreducible: [Block] = [
-    Block("bb0", .condBr("", "bb1", [], "bb2", [])),
-    Block("bb1", .condBr("", "bb2", [], "bb3", [])),
-    Block("bb2", .condBr("", "bb1", [], "bb3", [])),
-    Block("bb3", returnInstr),
-  ]
-
-  let chain: [Block] = [
-    Block("bb0", .br("bb1", [])),
-    Block("bb1", .br("bb2", [])),
-    Block("bb2", .br("bb3", [])),
-    Block("bb3", .br("bb4", [])),
-    Block("bb4", returnInstr),
-  ]
-
-  let ifDiamond: [Block] = [
-    Block("bb0", .condBr("", "bb1", [], "bb2", [])),
-    Block("bb1", .br("bb3", [])),
-    Block("bb2", .br("bb3", [])),
-    Block("bb3", returnInstr),
-  ]
-
-  let simpleLoop: [Block] = [
-    Block("bb0", .br("bb1", [])),
-    Block("bb1", .condBr("", "bb2", [], "bb3", [])),
-    Block("bb2", .br("bb1", [])),
-    Block("bb3", returnInstr),
-  ]
-
-  let loopWithIf: [Block] = [
-    Block("bb0", .br("bb1", [])),
-    Block("bb1", .condBr("", "bb2", [], "bb6", [])),
-    Block("bb2", .condBr("", "bb3", [], "bb4", [])),
-    Block("bb3", .br("bb5", [])),
-    Block("bb4", .br("bb5", [])),
-    Block("bb5", .br("bb1", [])),
-    Block("bb6", returnInstr),
-  ]
-
-  let loopWithTwoBackEdges: [Block] = [
-    Block("bb0", .br("bb1", [])),
-    Block("bb1", .condBr("", "bb2", [], "bb5", [])),
-    Block("bb2", .condBr("", "bb3", [], "bb4", [])),
-    Block("bb3", .br("bb1", [])),
-    Block("bb4", .br("bb1", [])),
-    Block("bb5", returnInstr),
-  ]
-
-  let nestedLoop: [Block] = [
-    Block("bb0", .br("bb1", [])),
-    Block("bb1", .condBr("", "bb2", [], "bb5", [])),
-    Block("bb2", .condBr("", "bb3", [], "bb4", [])),
-    Block("bb3", .br("bb2", [])),
-    Block("bb4", .br("bb1", [])),
-    Block("bb5", returnInstr),
-  ]
-
   func testInducesReducibleCFG() {
     XCTAssertEqual(induceReducibleCFG(irreducible), false)
-    XCTAssertEqual(induceReducibleCFG(chain), true)
-    XCTAssertEqual(induceReducibleCFG(ifDiamond), true)
-    XCTAssertEqual(induceReducibleCFG(simpleLoop), true)
-    XCTAssertEqual(induceReducibleCFG(loopWithIf), true)
-    XCTAssertEqual(induceReducibleCFG(loopWithTwoBackEdges), true)
-    XCTAssertEqual(induceReducibleCFG(nestedLoop), true)
+    for cfg in reducibleExamples {
+      XCTAssertEqual(induceReducibleCFG(cfg), true)
+    }
   }
 
   func testFindLoops() {
@@ -86,8 +91,148 @@ final class CFGPreprocessingTests: XCTestCase {
                                                 body: ["bb2", "bb3", "bb4"])])
   }
 
+  func testUnloopNested() {
+    func o(_ name: String) -> Operand {
+      return Operand(name, .namedType("Int"))
+    }
+    var nestedLoop: [Block] = [
+      Block("bb0", ["%0", "%1"],
+            .br("bb1", [o("%0"), o("%1")])),
+      Block("bb1", ["%2", "%3"], [
+              OperatorDef(Result(["%4"]), .builtin("something1", [o("%2")], .namedType("Int")), nil),
+            ],
+            .condBr("%4", "bb2", [o("%3")], "bb5", [])),
+      Block("bb2", ["%5"], [
+              OperatorDef(Result(["%6"]), .builtin("something2", [o("%5")], .namedType("Int")), nil),
+            ],
+            .condBr("%6", "bb3", [], "bb4", [])),
+      Block("bb3", [], [
+              OperatorDef(Result(["%7"]), .builtin("magic", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%8"]), .builtin("more_magic", [], .namedType("Int")), nil),
+            ],
+            .condBr("%8", "bb2", [o("%7")], "bb5", [])),
+      Block("bb4", [], [
+              OperatorDef(Result(["%9"]), .builtin("iter_cond", [o("%2")], .namedType("Int")), nil),
+              OperatorDef(Result(["%10"]), .builtin("super_magic", [], .namedType("Int")), nil),
+            ],
+            .br("bb1", [o("%9"), o("%10")])),
+      Block("bb5", .return(o("%1"))),
+    ]
+    unloop(&nestedLoop)
+    let expectedOutput: [Block] = [
+      Block("bb0", ["%0", "%1"],
+            .br("bb1", [o("%0"), o("%1")])),
+      Block("bb1", ["%2", "%3"], [
+              OperatorDef(Result(["%4"]), .builtin("something1", [o("%2")], .namedType("Int")), nil),
+            ],
+            .condBr("%4", "bb2", [o("%3")], "bb5", [])),
+      Block("bb2", ["%5"], [
+              OperatorDef(Result(["%6"]), .builtin("something2", [o("%5")], .namedType("Int")), nil),
+            ],
+            .condBr("%6", "bb3", [], "bb4", [])),
+      Block("bb3", [], [
+              OperatorDef(Result(["%7"]), .builtin("magic", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%8"]), .builtin("more_magic", [], .namedType("Int")), nil),
+            ],
+            .condBr("%8", "bb2_bridge_1", [o("%7")], "bb5", [])),
+      Block("bb4", [], [
+              OperatorDef(Result(["%9"]), .builtin("iter_cond", [o("%2")], .namedType("Int")), nil),
+              OperatorDef(Result(["%10"]), .builtin("super_magic", [], .namedType("Int")), nil),
+            ],
+            .br("bb1_bridge_10", [o("%9"), o("%10")])),
+      Block("bb5", .return(o("%1"))),
+
+      // First unloop
+      Block("bb3_0", [], [
+              OperatorDef(Result(["%7"]), .builtin("magic", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%8"]), .builtin("more_magic", [], .namedType("Int")), nil),
+            ],
+            .condBr("%8", "bb2_final_3", [o("%7")], "bb5", [])),
+      Block("bb2_bridge_1", ["%unused_2"], [
+              OperatorDef(Result(["%5"]), .builtin("anyInhabitant", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%6"]), .builtin("something2", [o("%5")], .namedType("Int")), nil),
+            ],
+            .condBr("%6", "bb3_0", [], "bb4_unreachable", [])),
+      Block("bb4_unreachable", .unreachable),
+      Block("bb2_final_3", ["%5"], [
+              OperatorDef(Result(["%6"]), .builtin("something2", [o("%5")], .namedType("Int")), nil),
+            ],
+            .condBr("%6", "bb3_unreachable", [], "bb4", [])),
+      Block("bb3_unreachable", .unreachable),
+
+      // Second unloop
+      Block("bb2_4", ["%5"], [
+              OperatorDef(Result(["%6"]), .builtin("something2", [o("%5")], .namedType("Int")), nil),
+            ],
+            .condBr("%6", "bb3_7", [], "bb4_9", [])),
+      Block("bb2_bridge_1_5", ["%unused_2"], [
+              OperatorDef(Result(["%5"]), .builtin("anyInhabitant", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%6"]), .builtin("something2", [o("%5")], .namedType("Int")), nil),
+            ],
+            .condBr("%6", "bb3_0_8", [], "bb4_unreachable", [])),
+      Block("bb2_final_3_6", ["%5"], [
+              OperatorDef(Result(["%6"]), .builtin("something2", [o("%5")], .namedType("Int")), nil),
+            ],
+            .condBr("%6", "bb3_unreachable", [], "bb4_9", [])),
+      Block("bb3_7", [], [
+              OperatorDef(Result(["%7"]), .builtin("magic", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%8"]), .builtin("more_magic", [], .namedType("Int")), nil),
+            ],
+            .condBr("%8", "bb2_bridge_1_5", [o("%7")], "bb5", [])),
+      Block("bb3_0_8", [], [
+              OperatorDef(Result(["%7"]), .builtin("magic", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%8"]), .builtin("more_magic", [], .namedType("Int")), nil),
+            ],
+            .condBr("%8", "bb2_final_3_6", [o("%7")], "bb5", [])),
+      Block("bb4_9", [], [
+              OperatorDef(Result(["%9"]), .builtin("iter_cond", [o("%2")], .namedType("Int")), nil),
+              OperatorDef(Result(["%10"]), .builtin("super_magic", [], .namedType("Int")), nil),
+            ],
+            .br("bb1_final_13", [o("%9"), o("%10")])),
+      Block("bb1_bridge_10", ["%unused_11", "%unused_12"], [
+              OperatorDef(Result(["%2"]), .builtin("anyInhabitant", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%3"]), .builtin("anyInhabitant", [], .namedType("Int")), nil),
+              OperatorDef(Result(["%4"]), .builtin("something1", [o("%2")], .namedType("Int")), nil),
+            ],
+            .condBr("%4", "bb2_4", [o("%3")], "bb5_unreachable", [])),
+      Block("bb5_unreachable", .unreachable),
+      Block("bb1_final_13", ["%2", "%3"], [
+              OperatorDef(Result(["%4"]), .builtin("something1", [o("%2")], .namedType("Int")), nil),
+            ],
+            .condBr("%4", "bb2_unreachable", [o("%3")], "bb5", [])),
+      Block("bb2_unreachable", ["%5"], .unreachable),
+    ]
+    XCTAssertEqual(nestedLoop, expectedOutput)
+  }
+
+  func testUnloopProperties() {
+    for templateCFG in reducibleExamples {
+      var cfg = clone(templateCFG)
+      let hasLoops = !findLoops(cfg).isEmpty
+      unloop(&cfg)
+      if !hasLoops {
+        XCTAssertEqual(cfg, templateCFG)
+      } else {
+        guard induceReducibleCFG(cfg)! else {
+          XCTFail("Unlooped graph is irreducible!")
+          continue
+        }
+        XCTAssertTrue(findLoops(cfg).isEmpty)
+      }
+    }
+  }
+
+  func clone(_ cfg: [Block]) -> [Block] {
+    return cfg.map {
+      Block($0.identifier, $0.arguments, $0.operatorDefs, $0.terminatorDef)
+    }
+  }
+
   static var allTests = [
     ("testInducesReducibleCFG", testInducesReducibleCFG),
+    ("testFindLoops", testFindLoops),
+    ("testUnloopNested", testUnloopNested),
+    ("testUnloopProperties", testUnloopProperties),
   ]
 }
 
@@ -95,4 +240,19 @@ fileprivate extension Block {
   convenience init(_ name: String, _ terminator: Terminator) {
     self.init(name, [], [], TerminatorDef(terminator, nil))
   }
+
+  convenience init(_ name: String, _ args: [String], _ terminator: Terminator) {
+    self.init(name, args, [], terminator)
+  }
+
+  convenience init(_ name: String, _ args: [String], _ body: [OperatorDef], _ terminator: Terminator) {
+    self.init(name, args.map{ Argument($0, .namedType("Int")) }, body, TerminatorDef(terminator, nil))
+  }
 }
+
+extension Loop: Equatable {
+  public static func ==(_ a: Loop, _ b: Loop) -> Bool {
+    return a.header == b.header && a.body == b.body
+  }
+}
+
